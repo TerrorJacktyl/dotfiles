e66c12282f3647cd20776e0279ba0b705c3935bb
Jack Zezula <jackzezula@tuta.io>
2022-02-03 18:04:23 +1100

    Add hack font for doom

--- configs/doom/doom.sh
+++ configs/doom/doom.sh
@@ -3,6 +3,9 @@ DOOM_BIN_PATH="$HOME/.emacs.d/bin"
 # install on mac
 brew tap jimeh/emacs-builds
 brew install --cask emacs-app-good
+# install my favourite font
+brew install homebrew/cask-fonts/font-hack
+
 # add doom to path
 if [[ ":$PATH:" == *":$DOOM_BIN_PATH:"* ]]; then
   echo "Your PATH contains doom."
