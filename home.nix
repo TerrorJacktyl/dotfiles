d44e120a0d9e09858102361f5a364463583d06e3
Jack Zezula <jack.z@canva.com>
2023-03-07 21:44:43 +1100

    Initial setup

--- home.nix
+++ home.nix
@@ -2,37 +2,46 @@
 
 {
 
+  fonts.fontconfig.enable = true;
+
   home = {
-    username = "apearce";
-    homeDirectory = "/Users/apearce";
+    username = "jack.z";
+    homeDirectory = "/Users/jack.z";
     # Specify packages not explicitly configured below
     packages = with pkgs; [
-      colima
       docker
-      entr
       fd
-      httpie
-      hyperfine
-      imagemagick
-      jq
-      juju
-      kubectl
       neovim
-      nodejs
-      pandoc
       ripgrep
+      starship
       tree
       tree-sitter
       youtube-dl
-      yq
+      (pkgs.nerdfonts.override {fonts = ["FiraCode" "DroidSansMono"]; })
     ];
-    sessionVariables = {
-      EDITOR = "hx";
-    };
     stateVersion = "21.11";
   };
 
   programs = {
+    alacritty = {
+      enable = true;
+      settings = {
+        cursor.style = "Beam";
+        font = {
+          family = "DroidSansMono";
+          size = 18;
+        };
+        prompt = "echo (starship prompt)";
+        # Change the default shell to home-manager's fish rather than the system default
+        shell.program = "${config.home.profileDirectory}/bin/fish";
+        window.dimensions = {
+          lines = 50;
+          columns = 100;
+          opacity = 0.95;
+        };
+      };
+    };
+
     bat = {
       enable = true;
       config = {
@@ -46,11 +55,6 @@
       nix-direnv = {
         enable = true;
       };
-    };
-
-    exa = {
-      enable = true;
-      enableAliases = true;
     };
 
     fish = {
@@ -69,15 +73,6 @@
             sha256 = "sha256-0rnd8oJzLw8x/U7OLqoOMQpK81gRc7DTxZRSHxN9YlM";
           };
         }
-        {
-          name = "fish-kubectl-completions";
-          src = pkgs.fetchFromGitHub {
-            owner = "evanlucas";
-            repo = "fish-kubectl-completions";
-            rev = "ced676392575d618d8b80b3895cdc3159be3f628";
-            sha256 = "sha256-OYiYTW+g71vD9NWOcX1i2/TaQfAg+c2dJZ5ohwWSDCc";
-          };
-        }
         # Need this when using Fish as a default macOS shell in order to pick
         # up ~/.nix-profile/bin
         {
@@ -90,34 +85,13 @@
           };
         }
       ];
-      shellInit = ''
-        # Set syntax highlighting colours; var names defined here:
-        # http://fishshell.com/docs/current/index.html#variables-color
-        set fish_color_normal normal
-        set fish_color_command white
-        set fish_color_quote brgreen
-        set fish_color_redirection brblue
-        set fish_color_end white
-        set fish_color_error -o brred
-        set fish_color_param brpurple
-        set fish_color_comment --italics brblack
-        set fish_color_match cyan
-        set fish_color_search_match --background=brblack
-        set fish_color_operator cyan
-        set fish_color_escape white
-        set fish_color_autosuggestion brblack
-      '';
       interactiveShellInit = ''
         # Activate the iTerm 2 shell integration
         iterm2_shell_integration
-
-        # Pick up conda installation
-        if test -x /Users/apearce/.mambaforge/bin/conda
-          eval /Users/apearce/.mambaforge/bin/conda "shell.fish" "hook" $argv | source
-        end
+        # Manually load starship
+        starship init fish | source
       '';
       shellAliases = {
-        ipython = "ipython --no-banner";
         nvim = "nvim -p";
         vim = "vim -p";
         rm = "rm -i";
@@ -130,51 +104,11 @@
       # An abbreviation will expand after <space> or <Enter> is hit
       shellAbbrs = {
         b = "bat";
-        ip = "ipython";
         g = "git";
-        ga = "git add";
-        gap = "git add -p";
-        gb = "git branch";
-        gc = "git commit";
-        gca = "git commit --amend";
-        gcan = "git commit --amend --no-edit";
-        gcm = "git commit -m";
-        gcl = "git clone";
-        gd = "git diff";
-        gds = "git diff --staged";
-        gl = "git prettylog";
-        gp = "git push";
-        gpf = "git push --force-with-lease";
-        gpl = "git pull";
-        gplp = "git pull --prune";
-        gr = "git restore";
-        grb = "git rebase";
-        grba = "git rebase --abort";
-        grbc = "git rebase --continue";
-        grbi = "git rebase -i";
-        gs = "git status -s -b";
-        gst = "git stash";
-        gstp = "git stash pop";
-        gsts = "git stash show -p";
-        gstx = "git stash drop";
-        gsw = "git switch";
-        gswc = "git switch -c";
-        gswm = "git switch main";
-        h = "http";
-        hme = "home-manager --flake ~/.config/nixpkgs edit";
-        hms = "home-manager --flake ~/.config/nixpkgs switch";
-        k = "kubectl";
-        ka = "kubectl apply -f";
-        kcgc = "kubectl config get-contexts";
-        kcuc = "kubectl config use-context";
-        kd = "kubectl describe";
-        kg = "kubectl get";
-        kl = "kubectl logs";
-        kr = "kubectl run -i --tty";
-        m = "make";
+        hme = "home-manager -f ~/dotfiles/home.nix edit";
+        hms = "home-manager -f ~/dotfiles/home.nix switch";
         n = "nvim";
         o = "open";
-        p = "python3";
       };
       functions = {
         ctrlp = {
@@ -193,11 +127,19 @@
           description = "Greeting to show when starting a fish shell";
           body = "";
         };
+        fish_mode_prompt = {
+          description = "Vim mode in fish";
+          body = "fish_default_mode_prompt";
+        };
         fish_user_key_bindings = {
           description = "Set custom key bindings";
           body = ''
+            # Add shortcuts for functions defined in the above config
             bind \cp ctrlp
             bind \cl 'ctrlp --hidden'
+
+            # Use vim bindings (--no-erase preserves the above shortcuts)
+            fish_vi_key_bindings insert
           '';
         };
         mkdcd = {
@@ -220,7 +162,6 @@
       enable = true;
       settings = {
         aliases = {
-          co = "pr checkout";
           pv = "pr view";
         };
         git_protocol = "ssh";
@@ -229,9 +170,26 @@
 
     git = {
       enable = true;
-      userName = "Alex Pearce";
-      userEmail = "alex@alexpearce.me";
+      userName = "Jack Zezula ";
+      userEmail = "jack.z@canva.com";
       aliases = {
+        a = "add";
+        b = "branch";
+        cm = "commit";
+        cma = "commit --amend";
+        cman = "commit --amend --no-edit";
+        cmm = "commit -m";
+        co = "checkout";
+        cob = "checkout -b";
+        d = "diff";
+        ds = "diff --staged";
+        del = "branch -d";
+        lg = "!git log --pretty=format:\"%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]\" --abbrev-commit -30";
+        lgo = "log --oneline";
+        r = "rebase";
+        ri = "rebase -i";
+        s = "status";
+        st = "stash";
         prettylog = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
       };
       delta = {
@@ -283,16 +241,6 @@
         ".envrc"
         "environment.yaml"
       ];
-      includes = [
-        {
-          contents = {
-            user = {
-              email = "alex@mystic.ai";
-            };
-          };
-          condition = "gitdir:~/Mystic/";
-        }
-      ];
     };
 
     helix = {
@@ -312,14 +260,36 @@
       };
     };
 
-    home-manager.enable = true;
-
-    starship = {
+    home-manager = {
+      # This needs to be true for this entire config to take effect
       enable = true;
-      enableFishIntegration = true;
-      settings = {
-        add_newline = true;
-      };
+    };
+
+    # starship = {
+    #   enable = true;
+    #   enableFishIntegration = true;
+    #   settings = {
+    #     add_newline = false;
+    #   };
+    # };
+
+    tmux = {
+      enable = true;
+      baseIndex = 1;
+      historyLimit = 100500;
+      keyMode = "emacs";
+      prefix = "C-Space";
+      extraConfig = ''
+        # Reduce timeout to prevent impact on vim within tmux
+        set -sg escape-time 10
+        set -s escape-time 0
+
+        # Enable mouse mode
+        set-option -g mouse on
+      '';
+      plugins = [ pkgs.tmuxPlugins.yank ];
+      # Change the default shell to home-manager's fish rather than the system default
+      shell = "${config.home.homeDirectory}/.nix-profile/bin/fish";
     };
 
     zoxide = {
@@ -328,15 +298,15 @@
     };
   };
 
-  xdg.configFile."nix/nix.conf".text = ''
-    experimental-features = nix-command flakes
-  '';
-
   # FIXME The init.vim unconditionally installed by this module conflicts with
   # our init.lua, so we cannot use the module for now and must install the
   # configuration explicitly
   xdg.configFile.nvim = {
     source = ./config/neovim;
     recursive = true;
+  };
+
+  xdg.configFile."starship.toml" = {
+    source = ./config/starship.toml;
   };
 }
